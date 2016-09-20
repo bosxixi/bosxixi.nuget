4f19c2a02b729fb0abc37b5123913110fbc717c8
bosxixi <bosxixi@outlook.com>
2016-09-20 17:55:04 +0800

    add bosxixi.Data

---
+++ bosxixi.Data/IRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bosxixi.Data
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        Task<TEntity> GetAsync(int id);
+        TEntity Get(string id);
+        Task<TEntity> GetAsync(string id);
+        TEntity Get(Guid id);
+        Task<TEntity> GetAsync(Guid id);
+
+        IEnumerable<TEntity> GetAll();
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        DbSqlQuery<TEntity> SqlQuery(string sql, params object[] parameters);
+        DbEntityEntry Entry(TEntity entity);
+    }
+}
---
+++ bosxixi.Data/Repository.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace bosxixi.Data
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public TEntity Get(int id)
+        {
+            // Here we are working with a DbContext, not PlutoContext. So we don't have DbSets 
+            // such as Courses or Authors, and we need to use the generic Set() method to access them.
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public async Task<TEntity> GetAsync(int id)
+        {
+            // Here we are working with a DbContext, not PlutoContext. So we don't have DbSets 
+            // such as Courses or Authors, and we need to use the generic Set() method to access them.
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+
+        public TEntity Get(string id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public async Task<TEntity> GetAsync(string id)
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public TEntity Get(Guid id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public async Task<TEntity> GetAsync(Guid id)
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            // Note that here I've repeated Context.Set<TEntity>() in every method and this is causing
+            // too much noise. I could get a reference to the DbSet returned from this method in the 
+            // constructor and store it in a private field like _entities. This way, the implementation
+            // of our methods would be cleaner:
+            // 
+            // _entities.ToList();
+            // _entities.Where();
+            // _entities.SingleOrDefault();
+            // 
+            // I didn't change it because I wanted the code to look like the videos. But feel free to change
+            // this on your own.
+            return Context.Set<TEntity>().ToList();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await Context.Set<TEntity>().ToListAsync();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await Context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().SingleOrDefault(predicate);
+        }
+
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await Context.Set<TEntity>().SingleOrDefaultAsync(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Context.Set<TEntity>().Add(entity);
+            }
+            //Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Context.Set<TEntity>().Remove(entity);
+            }
+            //Context.Set<TEntity>().RemoveRange(entities);
+        }
+        public DbSqlQuery<TEntity> SqlQuery(string sql, params object[] parameters)
+        {
+            return Context.Set<TEntity>().SqlQuery(sql, parameters);
+        }
+
+        public DbEntityEntry Entry(TEntity entity)
+        {
+            return Context.Entry(entity);
+        }
+    }
+}
