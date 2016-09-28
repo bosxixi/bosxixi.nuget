23ae8f2ad963b606d1a578c0b6f2e9a2388761f6
bosxixi <bosxixi@outlook.com>
2016-09-28 14:52:45 +0800

    add json

---
+++ bosxixi.Extensions/Json.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace bosxixi.Extensions
+{
+    public static class Json
+    {
+        static public void SaveObjectToDiskAsJson(object obj, string path)
+        {
+            string directory = Path.GetDirectoryName(path);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(path, json);
+        }
+
+        static public T ReadJsonFileToObject<T>(T obj, string path)
+        {
+            string json = File.ReadAllText(path);
+            return (T)JsonConvert.DeserializeObject(json, obj.GetType());
+        }
+    }
+}
