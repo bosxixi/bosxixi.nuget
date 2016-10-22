94289c3158cd5258e424634ac8217a7094825f0f
bosxixi <bosxixi@outlook.com>
2016-10-23 02:50:27 +0800

    add PublicInstancePropertiesEqual

--- bosxixi.Extensions/Extension.cs
+++ bosxixi.Extensions/Extension.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -258,6 +259,24 @@ namespace bosxixi.Extensions
             {
                 action(item);
             }
+        }
+
+        public static bool PublicInstancePropertiesEqual<T>(this T self, T to, params string[] ignore) where T : class
+        {
+            if (self != null && to != null)
+            {
+                var type = typeof(T);
+                var ignoreList = new List<string>(ignore);
+                var unequalProperties =
+                    from pi in type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    where !ignoreList.Contains(pi.Name)
+                    let selfValue = type.GetProperty(pi.Name).GetValue(self, null)
+                    let toValue = type.GetProperty(pi.Name).GetValue(to, null)
+                    where selfValue != toValue && (selfValue == null || !selfValue.Equals(toValue))
+                    select selfValue;
+                return !unequalProperties.Any();
+            }
+            return self == to;
         }
     }
 }
